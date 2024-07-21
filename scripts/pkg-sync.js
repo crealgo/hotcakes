d8aeef6e63856ab2b105d8d2fc983b3d4b4bd3c9
Manas Chimpidi <manaschimpidi@gmail.com>
2024-07-21 13:39:12 -0500

    perf: update add package link

--- scripts/pkg-sync.js
+++ scripts/pkg-sync.js
@@ -1,34 +1,39 @@
+#!/usr/bin/env node
+
 import fs from 'node:fs';
-import childProcess from "node:child_process";
+import cp from "node:child_process";
+import getPackages from './util/get-packages';
 
-// find all packages
-const pkgs = fs.readdirSync("packages", { withFileTypes: true })
-	.filter(f => f.isDirectory())
-	.map(f => `${f.path}/${f.name}`)
+function main() {
+	// find all packages
+	const pkgs = getPackages();
 
-// add all MIT
-for (const pkg of pkgs) {
-	console.log(`-w ${pkg}`)
+	// add all MIT
+	for (const pkg of pkgs) {
+		console.log(`-w ${pkg}`)
 
-	// add package
-	if (!fs.existsSync(`${pkg}/package.json`)) {
-		console.log(`-w ${pkg} is not initialized, fixing...`)
-		childProcess.execSync(`npm -fw ${pkg} init `)
+		// add package
+		if (!fs.existsSync(`${pkg}/package.json`)) {
+			console.log(`-w ${pkg} is not initialized, fixing...`)
+			cp.execSync(`npm -fw ${pkg} init `)
+		}
+
+		// sync license, sync author
+		cp.execSync(`npm -w ${pkg} pkg set license=MIT`);
+		cp.execSync(`npm -w ${pkg} pkg set author="Crealgo, LLC <hello.crealgo@gmail.com> (https://crealgo.com)"`);
+		cp.execSync(`npm -w ${pkg} pkg delete files`);
+		cp.execSync(`npm -w ${pkg} pkg set files[]="dist"`);
+
+		// add tsconfig to all packages
+		const configString = JSON.stringify({ "extends": "../../tsconfig.json" }, null, 2);
+		fs.writeFileSync(`${pkg}/tsconfig.json`, configString);
+
+		// link package
+		cp.execSync(`cd ${pkg} && npm link`);
 	}
 
-	// sync license, sync author
-	childProcess.execSync(`npm -w ${pkg} pkg set license=MIT`)
-	childProcess.execSync(`npm -w ${pkg} pkg set author="Crealgo, LLC <hello.crealgo@gmail.com> (https://crealgo.com)"`)
-	childProcess.execSync(`npm -w ${pkg} pkg delete files`)
-	childProcess.execSync(`npm -w ${pkg} pkg set files[]="dist"`)
-
-	// add tsconfig to all packages
-	fs.writeFileSync(`${pkg}/tsconfig.json`, JSON.stringify({
-		"extends": "../../tsconfig.json"
-	}, null, 2))
+	// sync reposistory
+	cp.execSync(`npm update`)
 }
 
-// sync reposistory
-childProcess.execSync(`npm update`)
-
-
+main();
