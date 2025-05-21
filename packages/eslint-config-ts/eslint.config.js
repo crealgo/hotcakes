c1849f1a094c79cf64769a1b8dd198a019113164
Manas Chimpidi <manaschimpidi@gmail.com>
2025-05-20 21:37:54 -0500

    perf: refine package.json

--- packages/eslint-config-ts/eslint.config.js
+++ packages/eslint-config-ts/eslint.config.js
@@ -1,20 +1,6 @@
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import js from "@eslint/js";
-import stylistic from '@stylistic/eslint-plugin'
+import tseslint from 'typescript-eslint';
 
-const indentation = 4;
-
-export default defineConfig([{
-    extends: ["js/all"],
-    files: ['**/*.js'],
-    languageOptions: { globals: globals.browser },
-    plugins: {
-        "@stylistic": stylistic,
-        js,
-    },
-    rules: {
-        "@stylistic/indent": ["error", indentation],
-        "func-style": ["error", "declaration", { allowArrowFunctions: true }],
-    }
-}]);
+export default tseslint.config(
+	tseslint.configs.strict,
+	tseslint.configs.stylistic,
+)
